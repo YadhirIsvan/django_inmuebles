b348be08c1f63bd60d2f6db5e18a8697ffcd256c
ronaldo <ronaldo@kaliii.kaliii.com>
2025-03-08 20:20:43 -0600

    se creo el campo comentario

--- inmuebleslist_app/admin.py
+++ inmuebleslist_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Edificacion, Empresa
+from .models import Edificacion, Empresa, Comentario
 
 admin.site.register(Edificacion)
 admin.site.register(Empresa)
+admin.site.register(Comentario)
--- inmuebleslist_app/models.py
+++ inmuebleslist_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Empresa (models.Model): 
     nombre = models.CharField(max_length=250)
@@ -21,3 +22,13 @@ class Edificacion(models.Model):
     def __str__(self):
         return self.direccion
     
+class Comentario(models.Model):
+    calificacion = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    texto = models.CharField(max_length=200, null = True)
+    edificacion = models.ForeignKey(Edificacion, on_delete=models.CASCADE, related_name="comentarios")
+    active = models.BooleanField(default=True)
+    create =models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.calificacion)
